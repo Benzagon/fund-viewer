98d793aca992c96c47ab1ddb81a7b594b52cb832
Benzagon <gonzabenzaquen@gmail.com>
2024-03-10 18:31:05 -0300

    feat: seed data & show correct dataValues

--- src/components/dashboard/grids/UserGrid.tsx
+++ src/components/dashboard/grids/UserGrid.tsx
@@ -1,6 +1,7 @@
 import { authOptions } from "@/app/api/auth/[...nextauth]/options";
 import { DataCard } from "@/components/cards/DataCard";
 import { getServerSession } from "next-auth";
+import { percentage } from "@/lib/utils";
 
 const fetchTokenData = async (id: string) => {
     const res = await fetch(`http://localhost:3000/api/get-user-token-data/?id=${id}`);
@@ -22,15 +23,17 @@ const UserGrid = async () => {
     const fundId = session?.user.fundId;
 
     const tokenData = await fetchTokenData(id || '');
-    const tokenValue: {value: number} = await fetchTokenValue(fundId || '');
-    const {tokens} = tokenData.data;
+    const tokenValue: { value: number } = await fetchTokenValue(fundId || '');
+    const { tokens, usdInvested, tokenValEntry } = tokenData.data;
 
+    const currentAmmount = tokens * tokenValue.value;
+    const PNL = currentAmmount - usdInvested;
     return (
         <div className="grid gap-7">
             <div className="flex justify-between gap-2">
-                <DataCard name="Current amount" value={tokens * tokenValue.value} porcent={2.14}></DataCard>
-                <DataCard name="PNL" value={2409} porcent={-2.14}></DataCard>
-                <DataCard name="Token value" value={tokenValue.value} porcent={tokenValue.value/100 * 100}></DataCard>
+                <DataCard name="Current amount" value={currentAmmount} porcent={percentage(currentAmmount, usdInvested)}></DataCard>
+                <DataCard name="PNL" value={PNL} porcent={tokenValue.value / tokenValEntry}></DataCard>
+                <DataCard name="Token value" value={tokenValue.value} porcent={percentage(tokenValue.value,tokenValEntry)}></DataCard>
             </div>
         </div>
     );
