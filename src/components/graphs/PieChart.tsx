f33a56cf1adf28ef17d02470e57be2e1ce9e5969
Benzagon <gonzabenzaquen@gmail.com>
2024-10-20 19:41:47 -0300

    feat: add donut chart legend

--- src/components/graphs/PieChart.tsx
+++ src/components/graphs/PieChart.tsx
@@ -1,6 +1,19 @@
 'use client';
 import { DonutChart } from '@tremor/react';
 
+const colorsHex = ['#808080', '#EAB308', '#4B0082', '#EE82EE', '#FF00FF'];
+
+const AssetTag = ({name, value, color}: {name: string, value: string, color: string}) => {
+  return (
+    <div className='flex items-center justify-start gap-1 h-6 text-base'>
+      <p style={{ color }} className='text-3xl'>•</p>
+      <p>
+        <span className='font-normal'>{`${name}: `}</span>{value}
+      </p>
+    </div>
+  )
+}
+
 export default function PieChart({assets, nav}: {assets?: {name: string, value: number}[], nav: number}) {
   const valueFormatter = (number: number) =>
   `${((number / nav) * 100).toFixed(2)}%`;
@@ -10,7 +23,7 @@ export default function PieChart({assets, nav}: {assets?: {name: string, value: 
       <h2 className='font-medium 3xl:text-xl text-base'>Portfolio Diversity</h2>
       <h3 className='3xl:text-base text-sm text-fund-text-gray'>View asset allocation</h3>
       {assets && (
-        <div className="flex items-center justify-center font-medium 3xl:text-xl text-base">
+        <div className="flex sm:flex-row flex-col items-center justify-center font-medium 3xl:text-xl text-base gap-8 sm:gap-4 sm:pt-0 pt-4">
           <DonutChart
             data={assets}
             category="value"
@@ -18,9 +31,16 @@ export default function PieChart({assets, nav}: {assets?: {name: string, value: 
             label='Net Assets'
             valueFormatter={valueFormatter}
             showAnimation={true}
-            colors={['yellow', 'gray', 'indigo', 'violet', 'fuchsia']}
+            colors={['gray', 'yellow', 'indigo', 'violet', 'fuchsia']}
             className="3xl:w-64 3xl:h-64 w-48 h-48"
           />
+          <div className='flex flex-col h-full items-start'>
+            {
+              assets.map((asset, i) => (
+                <AssetTag key={i} name={asset.name} value={valueFormatter(asset.value)} color={colorsHex[i]} />
+              ))
+            }
+          </div>
         </div>
       )}
     </div>
