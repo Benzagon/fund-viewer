8371d59989f9450ac02ca3319d8516e48bc03187
Juli Kovalivker <julikovalivker@gmail.com>
2025-04-10 11:11:58 -0300

    feat: updateAsset

--- src/app/api/update-nav-value/handler.ts
+++ src/app/api/update-nav-value/handler.ts
@@ -1,6 +1,5 @@
 import prisma from "@/lib/prisma";
 
-//export async function updateAsset(value: number, name: string, fundId: number) {
 export async function updateAsset(assets: [string, number][], fundId: number) {
     for (let i = 0; i < assets.length; i++){
         try {
@@ -13,17 +12,22 @@ export async function updateAsset(assets: [string, number][], fundId: number) {
 
             if (!asset) {
                 throw new Error("Asset not found");
+                //console.log("No se encontro el asset: " + assets[i][0])
             }
+            else{
             
-            const updatedAsset = await prisma.nav.update({
-                where: {
-                    id: asset.id
-                },
-                data: {
-                    value: assets[i][1]
-                }
-            });
-            return updatedAsset;
+                const updatedAsset = await prisma.nav.update({
+                    where: {
+                        id: asset.id
+                    },
+                    data: {
+                        value: assets[i][1]
+                    }
+                });
+                
+                console.log(asset, i)
+                //return updatedAsset;
+            }
         }
         catch(e: unknown) {
             console.error(`Error updating asset: ${e}`);
--- src/app/api/update-nav-value/route.ts
+++ src/app/api/update-nav-value/route.ts
@@ -4,6 +4,9 @@ import {updateAsset} from "./handler";
 export async function POST(req: NextRequest) {
     const data: {assets: [string, number][], fundId: number} = await req.json();
     const {assets, fundId} = data;
+    console.log("Assets:", assets);
+    console.log("Fund ID:", fundId);
     const result = await updateAsset(assets, fundId);
     return NextResponse.json({result});
+    // return NextResponse.json({'hola':'hola'})
 }
