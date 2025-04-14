da0e615203cfac228da3f8df91b948f3028d0721
Benzagon <gonzabenzaquen@gmail.com>
2025-04-14 18:19:47 -0300

    test: comment

--- src/app/api/create-asset/route.ts
+++ src/app/api/create-asset/route.ts
@@ -3,7 +3,7 @@ import {createAsset} from "./handler";
 
 export async function POST(req: NextRequest) {
     const data: {asset: Asset, fundId: number} = await req.json();
-    const {asset, fundId} = data;
+    const {asset, fundId} = data; // Fetch data from req
     const result = await createAsset(asset as Asset, fundId);
     return NextResponse.json({result});
 }
